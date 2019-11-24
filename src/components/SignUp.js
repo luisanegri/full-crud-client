da26c64189947d354a8bfb13380a143ac3e8840a
luisanegri <luisaanegri@gmail.com>
2019-11-24 15:24:18 +0100

    login and sigup components, set up actions and reducer

---
+++ src/components/SignUp.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signup } from '../actions/signup';
+import { Button, Form, Col } from 'react-bootstrap/';
+
+class SignUp extends React.Component {
+  state = { email: '', password: '', username: '' };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.signup(
+      this.state.username,
+      this.state.email,
+      this.state.password
+    );
+    this.setState({ email: '', password: '', username: '' });
+  };
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Label>Sign up</Form.Label>
+          <Form.Row>
+            <Col>
+              <Form.Control
+                onChange={this.onChange}
+                value={this.state.username}
+                name="username"
+                placeholder="username"
+              />
+            </Col>
+            <Col>
+              <Form.Control
+                onChange={this.onChange}
+                value={this.state.email}
+                name="email"
+                placeholder="email"
+              />
+            </Col>
+            <Col>
+              {/* <Form.Control
+                onChange={this.onChange}
+                value={this.state.password}
+                name="password"
+                placeholder="password"
+              /> */}
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  name="password"
+                  placeholder="password"
+                  type="password"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Button type="submit">Sign Up</Button>
+            </Col>
+          </Form.Row>
+        </Form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = { signup };
+
+export default connect(null, mapDispatchToProps)(SignUp);
